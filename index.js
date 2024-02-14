06eb8edfbe156af5edf9b5c71fc9c5e7b511435f
steingartpeter <steingart.p2@gmail.com>
2024-02-14 20:00:59 +0100

    MYSQL OK 2024-02-14 003

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const mysql_conn = mysql.createConnection({
   database: "steingartp",
 });
 
+mysql_conn.query("SELECT * FROM steingartp.calendar WHERE rec_date = CURDATE();", (err, results, fields) => {
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
+});
+
 app.listen(9750, () => {
   console.log("Server is running on 9750");
 });
 
 app.get("/", function (req, res) {
   res.send("Hello from NODE API, with nodemon");
-  console.log("MYSQL CONNECTION:");
-  console.log(mysql_conn);
 });
