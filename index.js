2dfd7bc62fb057981271cd9c1b3669a613cbcaab
steingartpeter <steingart.p2@gmail.com>
2024-02-14 19:49:14 +0100

    MYSQL ADDED 2024-02-14 002

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
+
+const mysql_conn = mysql.createConnection({
+  host: "localhost",
+  user: "ax07057",
+  password: "an003722",
+  database: "steingartp",
+});
 
 app.listen(9750, () => {
   console.log("Server is running on 9750");
@@ -7,4 +15,6 @@ app.listen(9750, () => {
 
 app.get("/", function (req, res) {
   res.send("Hello from NODE API, with nodemon");
+  console.log("MYSQL CONNECTION:");
+  console.log(mysql_conn);
 });
